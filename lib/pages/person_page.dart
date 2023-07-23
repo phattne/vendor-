900f91ea6e2b5394ada8994b8f4862e4febf894b
Merlin <truonglong17120@gmail.com>
2023-07-23 14:35:35 +0700

    sửa lỗi dòng blue,thêm screenshot và thêm profile

--- lib/pages/person_page.dart
+++ lib/pages/person_page.dart
@@ -5,7 +5,7 @@ import 'package:vendor/share/constants.dart';
 import 'package:vendor/widgets/widgets.dart';
 
 class PersonPage extends StatefulWidget {
-  const PersonPage({super.key});
+  const PersonPage({Key? key}) : super(key: key);
 
   @override
   State<PersonPage> createState() => _PersonPageState();
@@ -13,43 +13,54 @@ class PersonPage extends StatefulWidget {
 
 class _PersonPageState extends State<PersonPage> {
   AuthService authService = AuthService();
+  String userName = "Phát Hữu"; // Replace with the user's actual name
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [
-        Title(color: Colors.black, child: Text("hi ," + "My Name"))
-      ]),
-      body: Column(
-        children: [
-          ListTile(
-            leading: Text(
-              "Oders",
-              style: Appstyle(Colors.red, 20, FontWeight.bold),
-            ),
-            title: IconButton(
-              onPressed: () {
-                nextScreen(context, OdersPage());
-              },
-              icon: Icon(
-                Icons.delivery_dining,
-                size: 40,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          ListTile(
-            title: Text(
-              "Log Out",
-              style: Appstyle(Colors.black, 30, FontWeight.w400),
-            ),
-            leading: IconButton(
-              onPressed: () {
-                authService.signOut();
-              },
-              icon: Icon(Icons.exit_to_app),
-            ),
+      appBar: AppBar(
+        title: Text('Profile'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              authService.signOut();
+            },
+            icon: Icon(Icons.exit_to_app),
           ),
         ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage(
+                  'assets/images/phat.jpg'), // Replace with the user's profile image
+            ),
+            SizedBox(height: 20),
+            Text(
+              userName,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Email: nguyenhuuphat@gmail.com", // Replace with the user's email
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                // Handle edit profile button press
+                // Navigate to an EditPersonPage or show a bottom sheet with editing options
+              },
+              child: Text('Edit Profile'),
+            ),
+          ],
+        ),
       ),
     );
   }
